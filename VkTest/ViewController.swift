d1ebe6d7c3d120cbbddfb687305746131f011b02
digitalby <yury@digitalby.me>
2020-10-29 13:14:12 +0300

    Initial commit

---
+++ VkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkTest
+//
+//  Created by Digital on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
