8d0744e8ff5d810f72dfce3716310d3ea5faa757
digitalby <yury@digitalby.me>
2020-10-29 23:55:05 +0300

    Add simple posts downloading for Browse VC

--- VkTest/Controller/BrowseViewController.swift
+++ VkTest/Controller/BrowseViewController.swift
@@ -6,8 +6,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class BrowseViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView?
+    let refreshControl = UIRefreshControl()
+    let userCredentialsHelper = UserCredentialsHelper()
+
+    let session = Session()
+
+    private(set) var posts = [String]()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        refreshPostsWithDefaultAuthPayload()
+    }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -25,6 +39,10 @@ class BrowseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView?.refreshControl = refreshControl
+        tableView?.dataSource = self
+        tableView?.delegate = self
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -34,10 +52,112 @@ class BrowseViewController: UIViewController {
 
 }
 
+//MARK: - Control views
+extension BrowseViewController {
+    @objc func didPullToRefresh(_ sender: Any?) {
+        refreshPostsWithDefaultAuthPayload()
+    }
+}
+
+//MARK: - Get data
+extension BrowseViewController {
+    func refreshPostsWithDefaultAuthPayload(resetExisting: Bool = true) {
+        if let payload = try? userCredentialsHelper.loadOAuthPayload() {
+            refreshPostsWithAuthPayload(payload, resetExisting: resetExisting)
+        }
+    }
+
+    func refreshPostsWithAuthPayload(_ authPayload: VKAuthPayload, resetExisting: Bool = true) {
+        session.cancelAllRequests()
+        refreshControl.beginRefreshing()
+        guard let userID = authPayload.userId else {
+            refreshControl.endRefreshing()
+            return
+        }
+        var components = URLComponents(string: #"https://api.vk.com/method/wall.get"#)!
+        components.queryItems = [
+            URLQueryItem(name: "v", value: "5.52"),
+            URLQueryItem(name: "access_token", value: authPayload.accessToken),
+            URLQueryItem(name: "owner_id", value: String(userID)),
+            URLQueryItem(name: "count", value: "20"),
+            URLQueryItem(name: "offset", value: resetExisting ? "0" : "\(posts.count)")
+        ]
+        let url: URL
+        do {
+            url = try components.asURL()
+        } catch {
+            refreshControl.endRefreshing()
+            return
+        }
+        session.request(url).validate().responseJSON { [weak self] response in
+            defer {
+                self?.refreshControl.endRefreshing()
+            }
+            if let error = response.error {
+                print("Request error \(error)")
+            } else if let value = response.value {
+                DispatchQueue.main.async {
+                    let json = JSON(value)
+                    let jsonResponse = json["response"]
+                    let jsonItems = jsonResponse["items"].array ?? []
+                    var postsCount = self?.posts.count ?? 0
+
+                    guard let self = self else {
+                        return
+                    }
+                    self.tableView?.beginUpdates()
+                    if resetExisting {
+                        self.posts = []
+                        for i in 0..<postsCount {
+                            self.tableView?.deleteRows(at: [IndexPath(row: i, section: 0)], with: .automatic)
+                        }
+                        postsCount = 0
+                    }
+                    for i in 0..<jsonItems.count {
+                        let item = jsonItems[i]
+                        let postText = item["text"].string ?? "<error>"
+                        let nextRow = self.posts.count
+                        self.posts.append(postText)
+                        self.tableView?.insertRows(at: [IndexPath(row: nextRow, section: 0)], with: .automatic)
+                    }
+                    self.tableView?.endUpdates()
+                }
+            }
+        }
+    }
+}
+
+//MARK: - Table View Data Source + Delegate
+extension BrowseViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.numberOfLines = 8
+        cell.textLabel?.text = posts[indexPath.row]
+
+        if indexPath.row + 1 == posts.count {
+            refreshPostsWithDefaultAuthPayload(resetExisting: false)
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
 //MARK: - Observer
 extension BrowseViewController {
     @objc func onCredentialsChanged(_ notification: Notification?) {
         if let payload = notification?.object as? VKAuthPayload {
+            refreshPostsWithAuthPayload(payload)
         }
     }
 
