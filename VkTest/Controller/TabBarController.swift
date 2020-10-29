c7c8b657050c6ed8d977d0122b1bddbfced50bb8
digitalby <yury@digitalby.me>
2020-10-29 19:43:10 +0300

    Add title update to TabViewController

--- VkTest/Controller/TabBarController.swift
+++ VkTest/Controller/TabBarController.swift
@@ -14,8 +14,26 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        delegate = self
         if (try? userCredentialsHelper.loadOAuthPayload()) == nil {
             navigationController?.performSegue(withIdentifier: "PresentLogin", sender: self)
         }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        updateTitle()
+    }
+
+    private func updateTitle() {
+        title = selectedViewController?.title
+    }
+}
+
+//MARK: - Tab bar delegate
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        updateTitle()
     }
 }
